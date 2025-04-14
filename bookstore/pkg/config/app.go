b38001d047a0008e85e688c522197b2a511a782b
michalski30 <m_steinke@web.de>
2025-04-14 15:12:01 +0200

    Basic CRUD operations implemented

---
+++ bookstore/pkg/config/app.go
@@ -0,0 +1,24 @@
+// connect with mysql database
+// goal of this file is to return a variable db which will help the other file to interact with the db
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var (
+	db *gorm.DB
+)
+
+func Connect() {
+	d, err := gorm.Open("mysql", "myuser:mypassword@/mydatabase?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic(err)
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
